68c0f5390597092224be76780805630d570b8bab
Rohit Mehta <rohitkumarmehta25@gmail.com>
2025-11-12 06:39:06 +0530

    added shrimmer on ai search

--- src/components/skeletons/AISearchLoader.tsx
+++ src/components/skeletons/AISearchLoader.tsx
@@ -4,7 +4,7 @@ export default function AIResultsSkeleton() {
   return (
     <div className="w-full h-full border-b border-dashed border-foreground/30">
       <div className="max-w-7xl mx-auto border-x overflow-hidden h-full">
-        <div className="flex justify-center items-center h-[60vh] w-full">
+        <div className="flex justify-center items-center h-[64vh] w-full">
           <Loader className="size-5 animate-spin transition-transform duration-700 ease-linear" />
           <span className="text-xl font-semibold tracking-tight">
             AI is analysing your search.
